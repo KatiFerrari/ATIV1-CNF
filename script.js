7e264ecf36e23361d1a9ef590438ef67b2b16daa
mariafer057 <mariafernandaalves057@gmail.com>
2024-11-11 21:37:28 -0400

    criei o arquivi server e mudei uma coisa no script

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
-window.onload = function() {
-    alert("Bem-vindo ao Meu Projeto!");
-};
+function exibirMensagemBoasVindas() { alert("Bem-vindo ao Meu Projeto!"); }
 
 function mudarTitulo() { document.getElementById("titulo").innerText = "Título Alterado!"; }
 
