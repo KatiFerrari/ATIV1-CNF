5be915055a175e091b496fa53e6df97406a97c18
mariafer057 <mariafernandaalves057@gmail.com>
2024-11-11 21:54:28 -0400

    adicionando botão

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ function exibirMensagemBoasVindas() { alert("Bem-vindo ao Meu Projeto!"); }
 
 function mudarTitulo() { document.getElementById("titulo").innerText = "Título Alterado!"; }
 
-document.body.onload = function() { let botao = document.createElement("button"); botao.innerText = "Clique para mudar o título"; botao.onclick = mudarTitulo; document.body.appendChild(botao); };
+document.getElementById("change-images").addEventListener("click", mudarTitulo)
+
+
 
 
 
