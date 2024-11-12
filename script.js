9ae11b09f89bfc80b0dddbf0ab483d283de866ce
mariafer057 <mariafernandaalves057@gmail.com>
2024-11-11 21:10:36 -0400

    Adiciona um botão ao corpo do HTML e vincula a função

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ window.onload = function() {
 
 function mudarTitulo() { document.getElementById("titulo").innerText = "Título Alterado!"; }
 
+document.body.onload = function() { let botao = document.createElement("button"); botao.innerText = "Clique para mudar o título"; botao.onclick = mudarTitulo; document.body.appendChild(botao); };
+
 
 
